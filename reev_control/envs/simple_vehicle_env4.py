449000d0c790edbaa44bf853d995970d38ddf65d
qingyuan <qingyuangao212@gmail.com>
2026-03-26 10:13:49 +0800

    update simulator output using mean in env info

--- reev_control/envs/simple_vehicle_env4.py
+++ reev_control/envs/simple_vehicle_env4.py
@@ -180,12 +180,10 @@ class SimpleVehicleEnv4(gym.Env):
             self.config['simulator_reward_vars']].to_dict(orient='list')
         reward_inputs['speed_seq'] = speed_seq
 
-        info.update(simulator_outputs_df.iloc[-1].to_dict())
+        info.update(simulator_outputs_df.mean().to_dict())
 
-        # Replace stored values with sequence statistics to match reward calculation
+        # Override EmsFuCns with sum instead of mean
         info['EmsFuCns'] = simulator_outputs_df['EmsFuCns'].sum()
-        info['EmsEngSpd'] = simulator_outputs_df['EmsEngSpd'].mean()
-        info['EmsEngTqFlywh'] = simulator_outputs_df['EmsEngTqFlywh'].mean()
 
         # compute step reward
         step_reward, reward_info = self.reward_fn(reward_inputs, done)
